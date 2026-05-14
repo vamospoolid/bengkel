9c091d9545f21e1e423c116e00dfb7cce4f14c83
vamospoolid <vamospoolid@gmail.com>
2026-05-15 02:36:30 +0800

    Deploy: 15/05/2026  2.36.30,32

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3002/api'
+  baseURL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+    ? 'http://localhost:3002/api' 
+    : `http://${window.location.hostname}:3002/api`
 });
 
 api.interceptors.request.use((config) => {
